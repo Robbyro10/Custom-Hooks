cbbca212ec6e7ab3540c6bb03c9f64f9aca6e4b7
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2022-09-05 12:38:33 -0400

    first commit

---
+++ useTodos.js
@@ -0,0 +1,57 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer";
+
+export const useTodos = ( initialState = {}) => {
+
+    const init = () => {
+        return JSON.parse( localStorage.getItem( 'todos' )) || [];
+    }
+    
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init )
+  
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ));
+    }, [todos])
+      
+  
+    const handleNewTodo = ( todo ) => {
+        for ( let state of todos) {
+            if ( state.description === todo.description) {
+                alert('Todo already added');
+                return;
+            }
+        }
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+  
+        dispatch( action );
+    }
+  
+    const handleDeleteTodo = ( id ) => {
+        dispatch( {
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+  
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
+    return {
+        ...todos,
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter( todo => !todo.done ).length,
+
+    }
+
+}
